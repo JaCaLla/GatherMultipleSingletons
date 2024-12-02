6ea9809913ec1810cb2cab666c857e42f6f542dc
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-02 16:34:07 +0100

    Blank iOS App project

---
+++ GatherMultipleSingletons/GatherMultipleSingletonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherMultipleSingletonsApp.swift
+//  GatherMultipleSingletons
+//
+//  Created by Javier Calatrava on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GatherMultipleSingletonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GatherMultipleSingletonsTests/GatherMultipleSingletonsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GatherMultipleSingletonsTests.swift
+//  GatherMultipleSingletonsTests
+//
+//  Created by Javier Calatrava on 2/12/24.
+//
+
+import Testing
+@testable import GatherMultipleSingletons
+
+struct GatherMultipleSingletonsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
